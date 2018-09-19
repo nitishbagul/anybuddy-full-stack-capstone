27676fea2439eb2dae341122fe3ca07372e3263d
Nitish Bagul <nitishbagul@Nitishs-MacBook-Pro.local>
2018-09-18 20:52:38 -0700

    added some unit tests

--- test/test-events.js
+++ test/test-events.js
@@ -10,7 +10,7 @@ const faker = require('faker');
 const mongoose = require('mongoose');
 var expect = chai.expect;
 
-var Items = require('../models/events.js');
+var Events = require('../models/events.js');
 const {
     TEST_DATABASE_URL
 } = require('../config');
@@ -33,12 +33,12 @@ function seedEventsData() {
     const seedData = [];
     for (let i = 1; i <= 10; i++) {
         seedData.push({
-            ownerId: faker.random.word(),
+            ownerId: "1234",
             ownerName: faker.name.firstName(),
             ownerEmail: faker.internet.email(),
-            ownerPhone: faker.random.phoneNumber(),
+            ownerPhone: faker.phone.phoneNumber(),
             eventTitle: faker.random.words(),
-            eventDate: faker.date.soon(),
+            eventDate: faker.date.recent(),
             eventTime: faker.random.word(),
             eventStreetAddress: faker.address.streetAddress(),
             eventCity: faker.address.city(),
@@ -52,18 +52,23 @@ function seedEventsData() {
     return Events.insertMany(seedData);
 }
 
-//Generate random data using faker
-function generateItemsData() {
+//Generate random for a single event
+function generateEventsData() {
     return {
-        itemName: faker.company.companyName(),
-        placeName: faker.address.city(),
-        placeId: faker.hacker.adjective(),
-        areaName: faker.address.country(),
-        areaId: faker.hacker.adjective(),
-        categoryName: faker.lorem.word(),
-        categoryId: faker.hacker.noun(),
-        loggedInUserName: "test-user",
-        loggedInUserId: "1234"
+        ownerId: "1234",
+        ownerName: faker.name.firstName(),
+        ownerEmail: faker.internet.email(),
+        ownerPhone: faker.random.phoneNumber(),
+        eventTitle: faker.random.words(),
+        eventDate: faker.date.soon(),
+        eventTime: faker.random.word(),
+        eventStreetAddress: faker.address.streetAddress(),
+        eventCity: faker.address.city(),
+        eventState: faker.address.state(),
+        eventCountry: faker.address.country(),
+        lat: faker.address.latitude(),
+        lng: faker.address.longitude(),
+        partnersRequired: faker.random.number()
     };
 }
 
@@ -88,56 +93,62 @@ describe('anybuddy-full-stack-capstone', function () {
         it('should return all events', function () {
             let res;
             return chai.request(app)
-                .get('/items/get/all/1234')
+                .get('/events/get/all/1234')
                 .then(function (_res) {
                     res = _res;
                     expect(res).to.have.status(200);
-                    return Items.count();
+                    return Events.count();
                 })
                 .then(function (count) {
-                    expect(res.body.itemsOutput).to.have.a.lengthOf(count);
+                    expect(res.body.eventsOutput).to.have.a.lengthOf(count);
                 });
         });
 
-        it('should return all the items with right fields', function () {
-            //let resBlogPost;
+        it('should return all the events with right fields', function () {
             return chai.request(app)
-                .get('/items/get/all/1234')
+                .get('/events/get/all/1234')
                 .then(function (res) {
-                    // console.log(res.body);
+                    console.log(res.body);
                     //Status 200
                     expect(res).to.have.status(200);
                     //Should be a json
                     expect(res).to.be.json;
                     //Should be array
-                    expect(res.body.itemsOutput).to.be.a('array');
+                    expect(res.body.eventsOutput).to.be.a('array');
 
-                    res.body.itemsOutput.forEach(function (item) {
-                        expect(item).to.be.a('object');
-                        expect(item).to.include.keys('itemName', 'areaName', 'areaId', 'placeName', 'placeId', 'categoryName', 'categoryId');
+                    res.body.eventsOutput.forEach(function (event) {
+                        expect(event).to.be.a('object');
+                        expect(event).to.include.keys('ownerId', 'ownerName', 'ownerEmail', 'ownerPhone', 'eventTitle', 'eventDate', 'eventTime', 'eventStreetAddress', 'eventCity', 'eventState', 'eventCountry', 'lat', 'lng', 'partnersRequired');
                     });
-                    resItem = res.body.itemsOutput[0];
-                    return Items.findById(resItem._id);
+                    resEvent = res.body.eventsOutput[0];
+                    return Events.findById(resEvent._id);
                 })
 
-                .then(function (item) {
-                    expect(resItem.itemName).to.equal(item.itemName);
-                    expect(resItem.areaName).to.equal(item.areaName);
-                    expect(resItem.areaId).to.equal(item.areaId);
-                    expect(resItem.placeName).to.equal(item.placeName);
-                    expect(resItem.placeId).to.equal(item.placeId);
-                    expect(resItem.categoryName).to.equal(item.categoryName);
-                    expect(resItem.categoryId).to.equal(item.categoryId);
+                .then(function (event) {
+                    expect(resEvent.ownerId).to.equal(event.ownerId);
+                    expect(resEvent.ownerName).to.equal(event.ownerName);
+                    expect(resEvent.ownerEmail).to.equal(event.ownerEmail);
+                    expect(resEvent.ownerPhone).to.equal(event.ownerPhone);
+                    expect(resEvent.eventTitle).to.equal(event.eventTitle);
+                    //expect(resEvent.eventDate).to.equal(event.eventDate);
+                    expect(resEvent.eventTime).to.equal(event.eventTime);
+                    expect(resEvent.eventStreetAddress).to.equal(event.eventStreetAddress);
+                    expect(resEvent.eventCity).to.equal(event.eventCity);
+                    expect(resEvent.eventState).to.equal(event.eventState);
+                    expect(resEvent.eventCountry).to.equal(event.eventCountry);
+                    expect(resEvent.lat).to.equal(event.lat);
+                    expect(resEvent.lng).to.equal(event.lng);
+                    expect(resEvent.partnersRequired).to.equal(event.partnersRequired);
                 });
         });
     });
     describe('POST endpoint', function () {
 
         it('should add a new item', function () {
-            const newItem = generateItemsData();
+            const newEvent = generateEventsData();
 
             return chai.request(app)
-                .post('/items/create')
+                .post('/events/create')
                 .send(newItem)
                 .then(function (res) {
                     expect(res).to.have.status(200);
@@ -168,60 +179,60 @@ describe('anybuddy-full-stack-capstone', function () {
 
     });
 
-    describe('PUT endpoint', function () {
-
-        it('should update item fields you send over', function () {
-            const updateItem = {
-                placeName: 'fofoplace',
-                placeId: 'fofoplaceid',
-                areaName: 'fofoarea',
-                areaId: 'fofoareaid'
-            };
-
-            return Items
-                .findOne()
-                .then(function (item) {
-                    updateItem.id = item._id;
-
-                    // make request then inspect it to make sure it reflects
-                    // data we sent
-                    return chai.request(app)
-                        .put(`/items/${item._id}`)
-                        .send(updateItem);
-                })
-                .then(function (res) {
-                    expect(res).to.have.status(204);
-
-                    return Items.findById(updateItem.id);
-                })
-                .then(function (itemElement) {
-                    expect(itemElement.placeName).to.equal(updateItem.placeName);
-                    expect(itemElement.placeId).to.equal(updateItem.placeId);
-                    expect(itemElement.areaName).to.equal(updateItem.areaName);
-                    expect(itemElement.areaId).to.equal(updateItem.areaId);
-                });
-        });
-    });
-
-    describe('DELETE endpoint', function () {
-
-        it('delete an item by id', function () {
-            let anyItem;
-
-            return Items
-                .findOne()
-                .then(function (_resItem) {
-                    anyItem = _resItem;
-                    return chai.request(app).delete(`/item/${anyItem._id}`);
-                })
-                .then(function (res) {
-                    expect(res).to.have.status(204);
-                    return Items.findById(anyItem._id);
-                })
-                .then(function (_resItem) {
-                    expect(_resItem).to.be.null;
-                });
-
-        });
-    });
+    //    describe('PUT endpoint', function () {
+    //
+    //        it('should update item fields you send over', function () {
+    //            const updateItem = {
+    //                placeName: 'fofoplace',
+    //                placeId: 'fofoplaceid',
+    //                areaName: 'fofoarea',
+    //                areaId: 'fofoareaid'
+    //            };
+    //
+    //            return Items
+    //                .findOne()
+    //                .then(function (item) {
+    //                    updateItem.id = item._id;
+    //
+    //                    // make request then inspect it to make sure it reflects
+    //                    // data we sent
+    //                    return chai.request(app)
+    //                        .put(`/items/${item._id}`)
+    //                        .send(updateItem);
+    //                })
+    //                .then(function (res) {
+    //                    expect(res).to.have.status(204);
+    //
+    //                    return Items.findById(updateItem.id);
+    //                })
+    //                .then(function (itemElement) {
+    //                    expect(itemElement.placeName).to.equal(updateItem.placeName);
+    //                    expect(itemElement.placeId).to.equal(updateItem.placeId);
+    //                    expect(itemElement.areaName).to.equal(updateItem.areaName);
+    //                    expect(itemElement.areaId).to.equal(updateItem.areaId);
+    //                });
+    //        });
+    //    });
+    //
+    //    describe('DELETE endpoint', function () {
+    //
+    //        it('delete an item by id', function () {
+    //            let anyItem;
+    //
+    //            return Items
+    //                .findOne()
+    //                .then(function (_resItem) {
+    //                    anyItem = _resItem;
+    //                    return chai.request(app).delete(`/item/${anyItem._id}`);
+    //                })
+    //                .then(function (res) {
+    //                    expect(res).to.have.status(204);
+    //                    return Items.findById(anyItem._id);
+    //                })
+    //                .then(function (_resItem) {
+    //                    expect(_resItem).to.be.null;
+    //                });
+    //
+    //        });
+    //    });
 });
